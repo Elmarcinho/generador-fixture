431461c027fea82673992f1ba66846e777e7053a
Elmarcinho <Elmarcinho23@gmail.com>
2026-06-13 09:13:27 -0400

    202606130913 Se gemero archivos necesario para subir repositorio GitHub

---
+++ config.example.js
@@ -0,0 +1,3 @@
+// Copia este archivo como config.js y reemplaza con tu token de football-data.org
+// Regístrate gratis en: https://www.football-data.org/client/register
+const FD_TOKEN = 'TU_TOKEN_AQUI';
